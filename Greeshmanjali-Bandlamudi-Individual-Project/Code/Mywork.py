8178a86b63ea35a1e5c39ba25565fcacc2752aef
Pranay-yanarp <pranaythetitan@gmail.com>
2022-05-02 23:47:26 -0400

    1

--- Greeshmanjali-Bandlamudi-Individual-Project/Code/Mywork.py
+++ Greeshmanjali-Bandlamudi-Individual-Project/Code/Mywork.py
@@ -1,3 +1,84 @@
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+import pandas as pd
+
+import os
+
+import nltk # NLP
+import re
+
+import os
+
+path1 = os.path.split(os.getcwd())[0] + '/project_datasets'
+
+np.random.seed(0)
+
+Fake_df = pd.read_csv(path1+'/'+'Fake.csv')
+True_df = pd.read_csv(path1+'/'+'True.csv')
+
+
+Fake_df['target'] = 0
+True_df['target'] = 1
+
+df = pd.concat([True_df, Fake_df], ignore_index=True, sort=False)
+import random
+df = df.sample(len(df),ignore_index=True)
+
+df['text'] = df['title'] + ' ' + df['text']
+
+df.drop(['title', 'subject', 'date',], axis=1, inplace=True)
+
+# tokenize and cleaning
+
+#  1. apply to remove url
+def remove_url(text):
+    return re.sub(r'http\S+', '', text)
+
+df['text'] = df['text'].apply(remove_url)
+
+#============================================
+
+# 2. apply lower
+def to_lower(text):
+    return text.lower()
+
+df['text'] = df['text'].apply(to_lower)
+
+#============================================
+
+# 3. apply contractions
+
+# import sys
+# import subprocess
+
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'contractions'])
+
+# subprocess.call("pip install --upgrade " + 'pip', shell=True)
+
+import contractions
+
+def remove_contractions(text):
+    return ' '.join([contractions.fix(word) for word in text.split()])
+
+df['text'] = df['text'].apply(remove_contractions)
+
+
+#============================================
+
+# 4. apply remove punctuations
+
+def remove_punctuations(text):
+    return re.sub('\[[^]]*\]', '', text)
+
+df['text'] = df['text'].apply(remove_punctuations)
+
+
+
+
+
+
+
 import numpy as np
 import pandas as pd
 from sklearn import metrics
@@ -138,7 +219,8 @@ for epoch in range(N_EPOCHS):
     print("Epoch: {}, Train Loss: {:.4f}, Train Accuracy: {:.2f}%".format(epoch, train_loss_per_epoch,
                                                                           train_acc_per_epoch))
     print("Epoch: {}, Validation Loss: {:.4f}, Validation Accuracy: {:.2f}%\n".format(epoch, val_loss_per_epoch,
-# Plotting the graphs                                                                                val_acc_per_epoch))
+                                                                                      val_acc_per_epoch))
+# Plotting the graphs
 import matplotlib.pyplot as plt
 plt.plot(np.arange(1,4),validation_losses)
 plt.title('validation loss vs Epochs')
